6e9cc71383a9a162c12818d7008bbeb70eb76d88
prithivkanth <kanthprithiv@gmail.com>
2025-07-16 22:09:19 +0530

    enchanced controller

--- src/walle_controller/include/walle_controller/simple_controller.hpp
+++ src/walle_controller/include/walle_controller/simple_controller.hpp
@@ -5,6 +5,7 @@
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <Eigen/Core>
 #include <std_msgs/msg/float64_multi_array.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 
 
 class SimpleController : public rclcpp::Node
@@ -15,12 +16,23 @@ public:
 private:
     void velCallback(const geometry_msgs::msg::TwistStamped &msg);
 
+    void jointCallback(const sensor_msgs::msg::JointState &msg);
+
     rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
     rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub_;
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_sub_;
 
     double wheel_radius_;
     double wheel_separation_;
     Eigen::Matrix2d speed_conversion_;
+
+    double left_wheel_prev_pos_;
+    double right_wheel_prev_pos_;
+    rclcpp::Time prev_time_;
+
+    double x_;
+    double y_;
+    double theta_;
 };
 
 #endif // SIMPLE_CONTROLLER_HPP
--- src/walle_controller/src/simple_controller.cpp
+++ src/walle_controller/src/simple_controller.cpp
@@ -5,6 +5,11 @@ using std::placeholders::_1;
 
 
 SimpleController::SimpleController(const std::string &name) : Node(name)
+    , left_wheel_prev_pos_(0.0)
+    , right_wheel_prev_pos_(0.0)
+    , x_(0.0)
+    , y_(0.0)
+    , theta_(0.0)
 
 {
     declare_parameter("wheel_radius", 0.054);
@@ -16,8 +21,12 @@ SimpleController::SimpleController(const std::string &name) : Node(name)
     RCLCPP_INFO_STREAM(get_logger(), "the radius is " << wheel_radius_);
     RCLCPP_INFO_STREAM(get_logger(), "the seperation is " << wheel_separation_);
 
+    prev_time_ = get_clock()->now();
+
     wheel_cmd_pub_= create_publisher<std_msgs::msg::Float64MultiArray>("/simple_velocity_controller/commands", 10);
     vel_sub_= create_subscription<geometry_msgs::msg::TwistStamped>("/walle_controller/cmd_vel", 10, std::bind(&SimpleController::velCallback, this, _1));
+
+    joint_sub_ = create_subscription<sensor_msgs::msg::JointState>("/joint_states", 10, std::bind(&SimpleController::jointCallback, this, _1)); 
 
     speed_conversion_ << wheel_radius_/2, wheel_radius_/2, wheel_radius_/wheel_separation_, -wheel_radius_/wheel_separation_;
     RCLCPP_INFO_STREAM(get_logger(), "the speed conversion is " << speed_conversion_);
@@ -40,6 +49,38 @@ void SimpleController::velCallback (const geometry_msgs::msg::TwistStamped &msg)
 
 }
 
+
+void SimpleController::jointCallback(const sensor_msgs::msg::JointState &msg)
+{
+    double dp_left = msg.position.at(1) - left_wheel_prev_pos_;
+    double dp_right = msg.position.at(0) - right_wheel_prev_pos_;
+
+    rclcpp::Time msg_time = msg.header.stamp;
+    rclcpp::Duration dt = msg_time - prev_time_;
+
+    left_wheel_prev_pos_= msg.position.at(1);
+    right_wheel_prev_pos_= msg.position.at(0);
+    prev_time_ = msg.header.stamp;
+
+
+    double fi_left = dp_left/dt.seconds();
+    double fi_right = dp_right/dt.seconds();
+
+    double linear = (wheel_radius_ * fi_right + wheel_radius_ * fi_left)/2;
+    double angular =  (wheel_radius_ * fi_left - wheel_radius_ * fi_right)/wheel_separation_;
+
+
+     // Calculate the position increment
+    double d_s = (wheel_radius_ * dp_right + wheel_radius_ * dp_left) / 2;
+    double d_theta = (wheel_radius_ * dp_right - wheel_radius_ * dp_left) / wheel_separation_;
+    theta_ += d_theta;
+    x_ += d_s * cos(theta_);
+    y_ += d_s * sin(theta_);
+
+
+    RCLCPP_INFO_STREAM(get_logger(), "Linear : "<<linear<<" Angular : "<<angular);
+
+}
 
 int main(int argc, char* argv[])
 {
