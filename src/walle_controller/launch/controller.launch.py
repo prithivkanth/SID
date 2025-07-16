f9403ff381e1ef446dd8f5f539e98241d5602868
prithivkanth <kanthprithiv@gmail.com>
2025-07-16 19:08:49 +0530

    added color to the bot

--- src/walle_controller/launch/controller.launch.py
+++ src/walle_controller/launch/controller.launch.py
@@ -1,69 +1,86 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition, UnlessCondition
+from launch.conditions import UnlessCondition, IfCondition
 
 
 def generate_launch_description():
-
-
-    use_python_arg = DeclareLaunchArgument(
-        "use_python",
-        default_value="False"
+    
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="True",
     )
-
+    use_simple_controller_arg = DeclareLaunchArgument(
+        "use_simple_controller",
+        default_value="True",
+    )
+   
     wheel_radius_arg = DeclareLaunchArgument(
         "wheel_radius",
-        default_value="0.054"
+        default_value="0.054",
     )
-
     wheel_separation_arg = DeclareLaunchArgument(
         "wheel_separation",
-        default_value="0.31"
+        default_value="0.31",
     )
-
-    use_python = LaunchConfiguration("use_python")
+    
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_simple_controller = LaunchConfiguration("use_simple_controller")
+    
     wheel_radius = LaunchConfiguration("wheel_radius")
     wheel_separation = LaunchConfiguration("wheel_separation")
 
-
-    joint_state_broadcaster_spawner = Node (
-        package= "controller_manager",
-        executable="spawner",
-        arguments=
-        [
-            "joint_state_broadcaster",
-            "--controller-manager",
-            "/controller_manager"
-        ]
-    )
-
-
-    simpler_controller = Node(
+    joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=
-        [
-            "simple_velocity_controller",
+        arguments=[
+            "joint_state_broadcaster",
             "--controller-manager",
-            "/controller_manager"
+            "/controller_manager",
+        ],
+    )
+
+    wheel_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["walle_controller", 
+                   "--controller-manager", 
+                   "/controller_manager"
+        ],
+        condition=UnlessCondition(use_simple_controller),
+    )
+
+    simple_controller = GroupAction(
+        condition=IfCondition(use_simple_controller),
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["simple_velocity_controller", 
+                           "--controller-manager", 
+                           "/controller_manager"
+                ]
+            ),
+            Node(
+                package="walle_controller",
+                executable="simple_controller",
+                parameters=[
+                    {"wheel_radius": wheel_radius,
+                    "wheel_separation": wheel_separation,
+                    "use_sim_time": use_sim_time}],
+            ),
         ]
     )
 
-    simpler_controller_cpp= Node(
-        package= "walle_controller",
-        executable="simple_controller",
-        parameters=[{"wheel_radius": wheel_radius,
-                     "wheel_separation": wheel_separation}],
-        condition=UnlessCondition(use_python)
+    return LaunchDescription(
+        [
+            use_sim_time_arg,
+            use_simple_controller_arg,
+            wheel_radius_arg,
+            wheel_separation_arg,
+            joint_state_broadcaster_spawner,
+            wheel_controller_spawner,
+            simple_controller,
+        ]
     )
-
-    return LaunchDescription([
-        use_python_arg,
-        wheel_radius_arg,
-        wheel_separation_arg,
-        joint_state_broadcaster_spawner,
-        simpler_controller,
-        simpler_controller_cpp
-    ])
