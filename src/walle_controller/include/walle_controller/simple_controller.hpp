1642cba7e84d9388025ed509b9711646f62b3420
prithivkanth <kanthprithiv@gmail.com>
2025-07-17 21:04:45 +0530

    odometry broadcast

--- src/walle_controller/include/walle_controller/simple_controller.hpp
+++ src/walle_controller/include/walle_controller/simple_controller.hpp
@@ -5,7 +5,10 @@
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <Eigen/Core>
 #include <std_msgs/msg/float64_multi_array.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <tf2_ros/transform_broadcaster.h>
 
 
 class SimpleController : public rclcpp::Node
@@ -21,6 +24,8 @@ private:
     rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
     rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub_;
     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_sub_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+
 
     double wheel_radius_;
     double wheel_separation_;
@@ -29,10 +34,14 @@ private:
     double left_wheel_prev_pos_;
     double right_wheel_prev_pos_;
     rclcpp::Time prev_time_;
+    nav_msgs::msg::Odometry odom_msg_;
 
     double x_;
     double y_;
     double theta_;
+ // TF
+    std::unique_ptr<tf2_ros::TransformBroadcaster> transform_broadcaster_;
+    geometry_msgs::msg::TransformStamped transform_stamped_;
 };
 
 #endif // SIMPLE_CONTROLLER_HPP
--- src/walle_controller/src/simple_controller.cpp
+++ src/walle_controller/src/simple_controller.cpp
@@ -1,5 +1,7 @@
 #include "walle_controller/simple_controller.hpp"
 #include <Eigen/Geometry>
+#include <tf2/LinearMath/Quaternion.h>
+
 
 using std::placeholders::_1;
 
@@ -27,6 +29,7 @@ SimpleController::SimpleController(const std::string &name) : Node(name)
     vel_sub_= create_subscription<geometry_msgs::msg::TwistStamped>("/walle_controller/cmd_vel", 10, std::bind(&SimpleController::velCallback, this, _1));
 
     joint_sub_ = create_subscription<sensor_msgs::msg::JointState>("/joint_states", 10, std::bind(&SimpleController::jointCallback, this, _1)); 
+    odom_pub_ = create_publisher<nav_msgs::msg::Odometry>("/walle_controller/odom", 10);
 
     speed_conversion_ << wheel_radius_/2, wheel_radius_/2, wheel_radius_/wheel_separation_, -wheel_radius_/wheel_separation_;
     RCLCPP_INFO_STREAM(get_logger(), "the speed conversion is " << speed_conversion_);
@@ -79,8 +82,39 @@ void SimpleController::jointCallback(const sensor_msgs::msg::JointState &msg)
 
 
     RCLCPP_INFO_STREAM(get_logger(), "Linear : "<<linear<<" Angular : "<<angular);
+    RCLCPP_INFO_STREAM(get_logger(), "x : "<<x_<< ",y : "<<y_<<", theta : "<< theta_);
+
+
+        // Compose and publish the odom message
+    tf2::Quaternion q;
+    q.setRPY(0, 0, theta_);
+    odom_msg_.header.stamp = get_clock()->now();
+    odom_msg_.pose.pose.position.x = x_;
+    odom_msg_.pose.pose.position.y = y_;
+    odom_msg_.pose.pose.orientation.x = q.getX();
+    odom_msg_.pose.pose.orientation.y = q.getY();
+    odom_msg_.pose.pose.orientation.z = q.getZ();
+    odom_msg_.pose.pose.orientation.w = q.getW();
+    odom_msg_.twist.twist.linear.x = linear;
+    odom_msg_.twist.twist.angular.z = angular;
+    odom_pub_->publish(odom_msg_);
+
+
+        // TF
+    transform_stamped_.transform.translation.x = x_;
+    transform_stamped_.transform.translation.y = y_;
+    transform_stamped_.transform.rotation.x = q.getX();
+    transform_stamped_.transform.rotation.y = q.getY();
+    transform_stamped_.transform.rotation.z = q.getZ();
+    transform_stamped_.transform.rotation.w = q.getW();
+    transform_stamped_.header.stamp = get_clock()->now();
+    transform_broadcaster_->sendTransform(transform_stamped_);
+
 
 }
+
+
+
 
 int main(int argc, char* argv[])
 {
