b4ec10d9faa4a8e75f9ba726d7ddae4ed05b764b
prithivkanth <kanthprithiv@gmail.com>
2025-07-19 20:49:39 +0530

    added noisy controller

--- src/walle_controller/launch/controller.launch.py
+++ src/walle_controller/launch/controller.launch.py
@@ -1,8 +1,30 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import UnlessCondition, IfCondition
+
+
+
+def noisy_controller(context, *argc, **kwargs):
+    wheel_radius = float(LaunchConfiguration("wheel_radius").perform(context))
+    wheel_separation = float(LaunchConfiguration("wheel_separation").perform(context))
+    wheel_radius_error = float(LaunchConfiguration("wheel_radius_error").perform(context))
+    wheel_separation_error = float(LaunchConfiguration("wheel_separation_error").perform(context))
+
+    noisy_controller_cpp= Node(
+        package="walle_controller",
+        executable= "noisy_controller",
+        parameters=[
+            {"wheel_radius" : wheel_radius + wheel_radius_error,
+             "wheel_separation_error" : wheel_separation + wheel_separation_error}
+        ]
+    )
+
+    return [
+        noisy_controller_cpp
+    ]
+
 
 
 def generate_launch_description():
@@ -24,7 +46,14 @@ def generate_launch_description():
         "wheel_separation",
         default_value="0.31",
     )
-    
+    wheel_radius_error = DeclareLaunchArgument(
+        "wheel_radius_error",
+        default_value="0.005",
+    )
+    wheel_separation_error = DeclareLaunchArgument(
+        "wheel_separation_error",
+        default_value="0.02",
+    )
     use_sim_time = LaunchConfiguration("use_sim_time")
     use_simple_controller = LaunchConfiguration("use_simple_controller")
     
@@ -73,14 +102,20 @@ def generate_launch_description():
         ]
     )
 
+    noisy_controller_launch = OpaqueFunction(function=noisy_controller)
+
+
     return LaunchDescription(
         [
             use_sim_time_arg,
             use_simple_controller_arg,
             wheel_radius_arg,
             wheel_separation_arg,
+            wheel_radius_error,
+            wheel_separation_error,
             joint_state_broadcaster_spawner,
             wheel_controller_spawner,
             simple_controller,
+            noisy_controller_launch,
         ]
     )
