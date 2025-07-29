26e729acd085b88304f755ab4c456b95112227f2
prithivkanth <kanthprithiv@gmail.com>
2025-07-29 15:53:35 +0530

    added arduino control

--- src/walle_firmware/src/serial_reciever.cpp
+++ src/walle_firmware/src/serial_reciever.cpp
@@ -44,3 +44,12 @@ private:
     LibSerial::SerialPort esp32_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SerialReciever>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
