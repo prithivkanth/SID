d0f0359126feaf5d79df4487627b1f8d86916596
prithivkanth <kanthprithiv@gmail.com>
2025-08-04 22:54:41 +0530

    rviz

--- src/walle_localization/include/walle_localization/odometry_motion_model.hpp
+++ src/walle_localization/include/walle_localization/odometry_motion_model.hpp
@@ -1,36 +1,27 @@
 #ifndef ODOMETRY_MOTION_MODEL_HPP
 #define ODOMETRY_MOTION_MODEL_HPP
-#include <rclcpp/rclcpp.hpp>
-#include <nav_msgs/msg/odometry.hpp>
-#include <geometry_msgs/msg/pose_array.hpp>
+
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
 
 
-
-class OdometryMotionModel : public rclcpp::Node 
+class OdometryMotionModel : public rclcpp::Node
 {
 public:
-    OdometryMotionModel(const std::string & name);
-
-    void statePrediction();
-    void measurementUpdate();
+    OdometryMotionModel(const std::string& name);
 
 
 private:
-
-    void odomCallback(const nav_msgs::msg::Odometry & odom);
-
-
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-    rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr pose_array_pub_;
-
-
-    geometry_msgs::msg::PoseArray samples_;
+    void odomCallback(const nav_msgs::msg::Odometry &);
 
     double alpha1_;
     double alpha2_;
     double alpha3_;
     double alpha4_;
-    int no_samples_;
+    int64_t nr_samples_;
 
     double last_odom_x_;
     double last_odom_y_;
@@ -38,6 +29,10 @@ private:
 
     bool is_first_odom_;
 
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr pose_array_pub_;
+    geometry_msgs::msg::PoseArray samples_;
+    
 };
 
-#endif //ODOMETRY_MOTION_MODEL_HPP
+#endif // ODOMETRY_MOTION_MODEL_HPP
