061ebce0afddc508e3f072a0d13e00394583e105
prithivkanth <kanthprithiv@gmail.com>
2025-08-15 20:34:33 +0530

    speed monitoring

--- src/walle_utils/src/safety_stop.cpp
+++ src/walle_utils/src/safety_stop.cpp
@@ -1,6 +1,17 @@
+#include <math.h>
+#include <string>
+#include <memory>
+#include <chrono>
+#include <thread>
+
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "std_msgs/msg/bool.hpp"
+#include "twist_mux_msgs/action/joy_turbo.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 enum State
 {
@@ -12,12 +23,14 @@ enum State
 class SafetyStop : public rclcpp::Node 
 {
 public:
-    SafetyStop() : Node("safety_stop_node"), state_{State::FREE}
+    SafetyStop() : Node("safety_stop_node"), state_{State::FREE}, prev_state_(State::FREE)
     {
-        declare_parameter<double>("danger_distance", 0.2);
+        declare_parameter<double>("danger_distance", 0.25);
+        declare_parameter<double>("warning_distance", 0.60);
         declare_parameter<std::string>("scan_topic", "scan");
         declare_parameter<std::string>("safety_stop_topic", "safety_stop");
         danger_distance_ = get_parameter("danger_distance").as_double();
+        warning_distance_ = get_parameter("warning_distance").as_double();
         std::string scan_topic_ = get_parameter("scan_topic").as_string();
         std::string safety_stop_topic = get_parameter("safety_stop_topic").as_string();
 
@@ -26,37 +39,72 @@ public:
             std::bind(&SafetyStop::laserCallback, this, std::placeholders::_1));
         safety_stop_pub_ = create_publisher<std_msgs::msg::Bool>(safety_stop_topic, 10);
 
+        decrease_speed_client_ =rclcpp_action::create_client<twist_mux_msgs::action::JoyTurbo>(this, "joy_turbo_decrease");
+        increase_speed_client_ =rclcpp_action::create_client<twist_mux_msgs::action::JoyTurbo>(this, "joy_turbo_increase");
+
+        while(!decrease_speed_client_ -> wait_for_action_server(1s) && rclcpp::ok())
+        {
+            RCLCPP_WARN(get_logger(), "Action /joy_turbo_decrease not available! Waiting..");
+            std::this_thread::sleep_for(2s);
+        }
+
+        while(!increase_speed_client_ -> wait_for_action_server(1s) && rclcpp::ok())
+        {
+            RCLCPP_WARN(get_logger(), "Action /joy_turbo_increase not available! Waiting..");
+            std::this_thread::sleep_for(2s);
+        }
+
     }
 
 
 private:
-    double danger_distance_;
-    State state_;
+    double danger_distance_,warning_distance_;
+    State state_, prev_state_;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
     rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr safety_stop_pub_;
 
+    rclcpp_action::Client<twist_mux_msgs::action::JoyTurbo>::SharedPtr decrease_speed_client_;
+    rclcpp_action::Client<twist_mux_msgs::action::JoyTurbo>::SharedPtr increase_speed_client_;
+
     void laserCallback(const sensor_msgs::msg::LaserScan &msg)
     {
-        state_ = State::FREE;
-        for (const auto &   range : msg.ranges){
-            if(!std::isinf(range) && range <= danger_distance_)
+        state_= State::FREE;
+
+        for (const auto &range : msg.ranges)
+        {
+            if(!std::isinf(range) && range <= warning_distance_)
             {
-                state_ = State::DANGER;
-                break;
+                state_= State::WARNING;
+
+                if(range<= danger_distance_)
+                {
+                    state_ = State::DANGER;
+                    break;
+                }
             }
         }
 
-        std_msgs::msg::Bool is_safety_stop;
-        if(state_ == State::DANGER)
+        if(state_ != prev_state_)
         {
-            is_safety_stop.data = true;
-        }
-        else if (state_== State::FREE)
-        {
-            is_safety_stop.data = false;
-        }
+            std_msgs::msg::Bool is_safety_stop;
+            if(state_ == State::WARNING)
+            {
+                is_safety_stop.data = false;
+                decrease_speed_client_->async_send_goal(twist_mux_msgs::action::JoyTurbo::Goal());
+            }
+            else if(state_== State::DANGER)
+            {
+                is_safety_stop.data = true;
+            }
+            else if(state_==State::FREE)
+            {
+                is_safety_stop.data = false;
+                increase_speed_client_ ->async_send_goal(twist_mux_msgs::action::JoyTurbo::Goal());
+            }
 
-        safety_stop_pub_ ->publish(is_safety_stop);
+            prev_state_ = state_;
+            safety_stop_pub_ ->publish(is_safety_stop);
+        }
     }
 };
 
