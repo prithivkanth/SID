d3396c773e6670281530e5dce0a4c3864f611a77
prithivkanth <kanthprithiv@gmail.com>
2025-10-18 10:59:46 +0530

    dijkstra costmap

--- src/walle_planning/src/dijkstra_planner.cpp
+++ src/walle_planning/src/dijkstra_planner.cpp
@@ -15,7 +15,7 @@ DijkstraPlanner::DijkstraPlanner() : Node("dijkstra_node")
     map_qos.durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
 
     map_sub_ = create_subscription<nav_msgs::msg::OccupancyGrid>(
-        "/map", map_qos, std::bind(&DijkstraPlanner::mapCallback, this, std::placeholders::_1));
+        "/costmap/costmap", map_qos, std::bind(&DijkstraPlanner::mapCallback, this, std::placeholders::_1));
     pose_sub_ = create_subscription<geometry_msgs::msg::PoseStamped>(
         "/goal_pose", 10, std::bind(&DijkstraPlanner::goalCallback, this, std::placeholders::_1));
 
@@ -100,9 +100,9 @@ nav_msgs::msg::Path DijkstraPlanner::plan(const geometry_msgs::msg::Pose &start,
             GraphNode new_node = active_node + dir;
 
             if (std::find(visited_nodes.begin(), visited_nodes.end(), new_node) == visited_nodes.end() &&
-                poseOnMap(new_node) && map_->data.at(poseToCell(new_node)) == 0)
+                poseOnMap(new_node) && map_->data.at(poseToCell(new_node)) < 99 && map_->data.at(poseToCell(new_node)) >=0)
                 {
-                    new_node.cost = active_node.cost + 1;
+                    new_node.cost = active_node.cost + 1 + map_->data.at(poseToCell(new_node));
                     new_node.prev = std::make_shared<GraphNode>(active_node);
                     pending_nodes.push(new_node);
                     visited_nodes.push_back(new_node);
