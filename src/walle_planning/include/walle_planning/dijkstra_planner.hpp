f6f71494bb4812d558f2555602de846ea4b3f400
prithivkanth <kanthprithiv@gmail.com>
2025-10-31 14:08:37 +0530

    planner server

--- src/walle_planning/include/walle_planning/dijkstra_planner.hpp
+++ src/walle_planning/include/walle_planning/dijkstra_planner.hpp
@@ -2,6 +2,7 @@
 #define DIJKSTRA_PLANNER_HPP
 
 #include <memory>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/path.hpp"
@@ -9,6 +10,9 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/pose.hpp"
 
+#include "nav2_core/global_planner.hpp"
+#include "nav2_util/lifecycle_node.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
 
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
@@ -45,28 +49,33 @@ struct GraphNode
     
 };
 
-class DijkstraPlanner : public rclcpp :: Node
+class DijkstraPlanner : public nav2_core::GlobalPlanner
 {
 public:
-    DijkstraPlanner();
+    DijkstraPlanner() = default;
+    ~DijkstraPlanner() = default;
+
+    void configure(
+        const rclcpp_lifecycle::LifecycleNode::WeakPtr &parent, std::string name, 
+        std::shared_ptr<tf2_ros::Buffer> tf, std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+    
+    void cleanup() override;
+    void activate() override;
+    void deactivate() override;
+
+    nav_msgs::msg::Path createPlan(
+        const geometry_msgs::msg::PoseStamped & start,
+        const geometry_msgs::msg::PoseStamped & goal) override;
 
 private:
-    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
 
-    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_; 
-
-    nav_msgs::msg::OccupancyGrid::SharedPtr map_;
-    nav_msgs::msg::OccupancyGrid visited_map_;
-
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::Buffer> tf_;
+    nav2_util::LifecycleNode::SharedPtr node_;
+    nav2_costmap_2d::Costmap2D *costmap_;
+    std::string global_frame_,name_;
 
     void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr map);
     void goalCallback(const geometry_msgs::msg::PoseStamped::SharedPtr pose);
-
-    nav_msgs::msg::Path plan(const geometry_msgs::msg::Pose &start, const geometry_msgs::msg::Pose &goal);
 
     GraphNode worldToGrid(const geometry_msgs::msg::Pose & pose);
 
--- src/walle_planning/src/dijkstra_planner.cpp
+++ src/walle_planning/src/dijkstra_planner.cpp
@@ -6,70 +6,20 @@
 
 namespace walle_planning
 {
-DijkstraPlanner::DijkstraPlanner() : Node("dijkstra_node")
+
+void DijkstraPlanner::configure(
+  const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+  std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros)
 {
-    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(get_clock());
-    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+  node_ = parent.lock();
+  name_ = name;
+  tf_ = tf;
+  costmap_ = costmap_ros->getCostmap();
+  global_frame_ = costmap_ros->getGlobalFrameID();
 
-    rclcpp::QoS map_qos(10);
-    map_qos.durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
-
-    map_sub_ = create_subscription<nav_msgs::msg::OccupancyGrid>(
-        "/costmap/costmap", map_qos, std::bind(&DijkstraPlanner::mapCallback, this, std::placeholders::_1));
-    pose_sub_ = create_subscription<geometry_msgs::msg::PoseStamped>(
-        "/goal_pose", 10, std::bind(&DijkstraPlanner::goalCallback, this, std::placeholders::_1));
-
-    path_pub_ = create_publisher<nav_msgs::msg::Path>(
-        "/dijkstra/path", 10);
-    map_pub_ = create_publisher<nav_msgs::msg::OccupancyGrid>(
-        "/dijkstra/visited_map", 10);
+  
 }
-
-void DijkstraPlanner::mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr map)
-{
-    map_ = map;
-    visited_map_.header.frame_id =  map->header.frame_id;
-    visited_map_.info = map->info;
-    visited_map_.data = std::vector<int8_t>(visited_map_.info.height * visited_map_.info.width, -1);
-}
-void DijkstraPlanner::goalCallback(const geometry_msgs::msg::PoseStamped::SharedPtr pose)
-{
-    if(!map_)
-    {
-        RCLCPP_ERROR(get_logger(), "No map recieved");
-        return;
-    }
-
-    visited_map_.data = std::vector<int8_t>(visited_map_.info.height * visited_map_.info.width, -1);
-
-    geometry_msgs::msg::TransformStamped map_to_base_tf;
-
-    try
-    {
-        map_to_base_tf = tf_buffer_ ->lookupTransform(map_->header.frame_id, "base_footprint", tf2::TimePointZero);
-    }
-
-    catch(const tf2::TransformException & ex)
-    {
-        RCLCPP_ERROR(get_logger(), "Could not tranform from base to base_footprint");
-        return;
-    }
-
-    geometry_msgs::msg::Pose map_to_base_pose;
-    map_to_base_pose.position.x = map_to_base_tf.transform.translation.x;
-    map_to_base_pose.position.y = map_to_base_tf.transform.translation.y;
-    map_to_base_pose.orientation = map_to_base_tf.transform.rotation;
-
-    auto path = plan(map_to_base_pose, pose->pose);
-    if (!path.poses.empty()) {
-        RCLCPP_INFO(this->get_logger(), "Shortest path found!");
-        path_pub_->publish(path);
-    } else {
-        RCLCPP_WARN(this->get_logger(), "No path found to the goal.");
-    }
-
-}
-
 nav_msgs::msg::Path DijkstraPlanner::plan(const geometry_msgs::msg::Pose &start, const geometry_msgs::msg::Pose &goal)
 {
     std::vector<std::pair<int, int>> explore_directions = 
