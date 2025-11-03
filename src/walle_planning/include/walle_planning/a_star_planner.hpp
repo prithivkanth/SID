2c14989d58d4b0be9c661aa093974aadde0d4672
prithivkanth <kanthprithiv@gmail.com>
2025-11-03 14:12:58 +0530

    astar nav2 smoother

--- src/walle_planning/include/walle_planning/a_star_planner.hpp
+++ src/walle_planning/include/walle_planning/a_star_planner.hpp
@@ -1,21 +1,23 @@
-#ifndef DIJKSTRA_PLANNER_HPP
-#define DIJKSTRA_PLANNER_HPP
+#ifndef A_STAR_PLANNER_HPP
+#define A_STAR_PLANNER_HPP
 
+#include <string>
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/path.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "geometry_msgs/msg/point.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
-#include "geometry_msgs/msg/pose.hpp"
 
-
-#include "tf2_ros/buffer.h"
-#include "tf2_ros/transform_listener.h"
+#include "nav2_core/global_planner.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav2_util/robot_utils.hpp"
+#include "nav2_util/lifecycle_node.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "nav2_msgs/action/smooth_path.hpp"
 
 namespace walle_planning
 {
-
 struct GraphNode
 {
     int x;
@@ -24,60 +26,65 @@ struct GraphNode
     double heuristic;
     std::shared_ptr<GraphNode> prev;
 
-    GraphNode() : GraphNode(0,0){}
-    
+    GraphNode() : GraphNode(0,0) {}
+
     GraphNode(int in_x, int in_y) : x(in_x), y(in_y), cost(0){}
-    
-    bool operator>(const GraphNode & other ) const 
-    {
-        return cost + heuristic> other.cost + other.heuristic;
+
+    bool operator>(const GraphNode & other) const { 
+        return cost + heuristic > other.cost + other.heuristic;
     }
 
-    bool operator==(const GraphNode & other ) const 
-    {
+    bool operator==(const GraphNode & other) const {
         return x == other.x && y == other.y;
     }
 
-    GraphNode operator+(std::pair<int, int> const & other)
-    {
+    GraphNode operator+(std::pair<int, int> const & other) {
         GraphNode res(x + other.first, y + other.second);
         return res;
     }
-    
 };
 
-class AStarPlanner : public rclcpp :: Node
+class AStarPlanner : public nav2_core::GlobalPlanner
 {
 public:
-    AStarPlanner();
+  AStarPlanner() = default;
+  ~AStarPlanner() = default;
+
+  void configure(
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+    std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+
+  void cleanup() override;
+
+  void activate() override;
+
+  void deactivate() override;
+
+  nav_msgs::msg::Path createPlan(
+    const geometry_msgs::msg::PoseStamped & start,
+    const geometry_msgs::msg::PoseStamped & goal) override;
 
 private:
-    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  nav2_util::LifecycleNode::SharedPtr node_;
+  nav2_costmap_2d::Costmap2D * costmap_;
+  std::string global_frame_, name_;
+  double interpolation_resolution_;
 
-    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_; 
+  rclcpp_action::Client<nav2_msgs::action::SmoothPath>::SharedPtr smooth_client_;
 
-    nav_msgs::msg::OccupancyGrid::SharedPtr map_;
-    nav_msgs::msg::OccupancyGrid visited_map_;
+  bool poseOnMap(const GraphNode & node);
+  
+  GraphNode worldToGrid(const geometry_msgs::msg::Pose & pose);
+  
+  geometry_msgs::msg::Pose gridToWorld(const GraphNode & node);
+  
+  unsigned int poseToCell(const GraphNode & node);
 
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-
-    void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr map);
-    void goalCallback(const geometry_msgs::msg::PoseStamped::SharedPtr pose);
-
-    nav_msgs::msg::Path plan(const geometry_msgs::msg::Pose &start, const geometry_msgs::msg::Pose &goal);
-
-    GraphNode worldToGrid(const geometry_msgs::msg::Pose & pose);
-
-    bool poseOnMap(const GraphNode & node);
-    geometry_msgs::msg::Pose gridToWorld(const GraphNode & node);
-    unsigned int poseToCell(const GraphNode & node);
-
-    double manhanttanDistance(const GraphNode & node, const GraphNode & goal_node);
-
+  double manhattanDistance(const GraphNode &node, const GraphNode &goal_node);
 };
-}
 
-#endif //DIJKSTRA_PLANNER_HPP
+}  
+
+#endif  // A_STAR_PLANNER_HPP
